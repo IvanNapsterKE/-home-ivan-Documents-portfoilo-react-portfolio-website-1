581fec126cf1d771c9f1a73531da0193bf3eafaf
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 16:51:00 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
  import React from 'react'
 
- import Header from './components/header/Header'
- import About from './components/about/About'
+
 
 const App = () => {
   return (
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './header.css'
-
 export const Header = () => {
   return (
     <div>Header</div>
   )
 }
+
 
