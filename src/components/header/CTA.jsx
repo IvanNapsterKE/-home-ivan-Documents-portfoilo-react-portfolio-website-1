aa5a41bcb87459576372a1d12223cab8c390b133
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 19:29:40 +0300

    cv download

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CV from '../../assets/robots cv.txt'
+
+const CTA = () => {
+  return (
+    <div className="cta">
+
+        <a href="#">Download Resume</a>
+        <a href="#Contacts">Give me a Shout</a>
+    </div> 
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,16 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hi, My name is</h5>
+
+        <h1>Ivan Nguyo</h1>
+
+        <h5 className="text-light">Fullstack Developer</h5>  
+
+      </div>
+    </header>
   )
 }
 
