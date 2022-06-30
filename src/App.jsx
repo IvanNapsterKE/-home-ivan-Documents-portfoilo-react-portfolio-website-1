7fa697b5928885a9a54ceca421d8e3b5151a44ee
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 17:11:37 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
  import React from 'react'
 //  import Header from './components/header/Header'
  import Nav from './components/nav/Nav'
+ import About from './components/about/About'
+ import Experience from './components/experience/Experience'
+ import Portfolio from './components/portfolio/Portfolio'
+ import Services from './components/services/Services'
+ import Testimonials from './components/testimonials/Testimonials'
+ 
 
 
 const App = () => {
@@ -10,6 +16,8 @@ const App = () => {
 
         {/* <Header /> */}
         <Nav />
+        <About/>
+        <Experience/>
         
     </>
 
