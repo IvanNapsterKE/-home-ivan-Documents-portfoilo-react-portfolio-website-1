438a8c0da68d1c33c1d048d1b66a38f522683b65
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 17:01:53 +0300

    DLD HEAD

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
  import React from 'react'
- import Header from './components/header/Header'
+//  import Header from './components/header/Header'
  import Nav from './components/nav/Nav'
 
 
@@ -8,7 +8,7 @@ const App = () => {
 
     <>
 
-        <Header />
+        {/* <Header /> */}
         <Nav />
         
     </>
