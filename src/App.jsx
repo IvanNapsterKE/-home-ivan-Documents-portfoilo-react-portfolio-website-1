c032ea5c432a28694c787a6f187a75960e47f2ec
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 17:46:41 +0300

    body fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@
 
 
 
-//  import Header from './components/header/Header'
+ import Header from './components/header/Header'
  import Nav from './components/nav/Nav'
  import About from './components/about/About'
  import Experience from './components/experience/Experience'
  import Services from './components/services/Services'
-//  import Portfolio from './components/portfolio/Portfolio'
+ import Portfolio from './components/portfolio/Portfolio'
  import Testimonials from './components/testimonials/Testimonials'
  import Contacts from "./components/contacts/Contacts"
  import Footer from './components/footer/Footer'
@@ -19,12 +19,12 @@ const App = () => {
 
     <> 
     
-        {/* <Header /> */}
+        <Header />
         <Nav />
         <About /> 
         <Experience />
         <Services />
-        {/* <Portfolio /> */}
+        <Portfolio />
         <Testimonials />
         <Contacts />
         <Footer />
