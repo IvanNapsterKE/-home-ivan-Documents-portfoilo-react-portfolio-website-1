5937c6060800980a38d144421a8b75fff9643a41
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 23:56:21 +0300

    nav 1

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './nav.css'
 
+import {BiHomeCircle} from 'react-icons/bi'
+import {ImBriefcase} from 'react-icons/im'
+import {MdOutlineMiscellaneousServices} from 'react-icons/md'
+import {BsInfoCircleFill} from "react-icons/bs";
+import {BiMessageRoundedDetail} from "react-icons/bi"
+import {RiCustomerService2Line} from 'react-icons/ri'
+
+
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#about"><BiHomeCircle/></a>
+      <a href="#experience"><ImBriefcase/></a>
+      <a href="#services"><MdOutlineMiscellaneousServices/></a>
+      <a href="#portfolio"><BsInfoCircleFill/></a>
+      <a href="#testimonials"><BiMessageRoundedDetail/></a>
+      <a href="#contacts"><RiCustomerService2Line/></a>
+      
+    </nav>
   )
 }
 
