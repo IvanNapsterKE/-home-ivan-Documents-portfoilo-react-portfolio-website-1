27000b080d62f4e3ec567da96519c5a4b7bbc153
IvanNapsterKE <nguyoivan@gmail.com>
2022-06-30 16:57:47 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
  import React from 'react'
-
+ import Header from './components/header/Header'
+ import Nav from './components/nav/Nav'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
 
         <Header />
+        <Nav />
         
     </>
 
